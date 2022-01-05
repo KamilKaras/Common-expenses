47be634a1118eecb67d5f409d5c148d1e7d9c786
Kamil <kamil.karasiewicz11@gmail.com>
2022-01-05 11:05:41 +0100

    New breakdown cost app

---
+++ script.js
@@ -0,0 +1,97 @@
+var User = /** @class */ (function () {
+    function User(nickName, counter) {
+        this.nickName = nickName;
+        this.userId = counter;
+        this.balance = 0;
+    }
+    User.prototype.getNickName = function () {
+        return this.nickName;
+    };
+    User.prototype.getBalance = function () {
+        return this.balance.toString();
+    };
+    User.prototype.getId = function () {
+        return this.userId;
+    };
+    User.prototype.setBalance = function (change) {
+        this.balance += change;
+    };
+    return User;
+}());
+var usersList = [];
+var addUser = document.querySelector(".add-user-button");
+var addPayment = document.querySelector(".add-payment-button");
+var confirmPayment = document.querySelector('.accept-button');
+document.querySelector('.close-btn').addEventListener('click', function () {
+    document.querySelector(".popup").style.display = "none";
+    document.querySelector(".popup-input-box").value = "";
+});
+addUser.addEventListener('click', addNewUser);
+addPayment.addEventListener('click', addNewPayment);
+confirmPayment.addEventListener('click', CalculatePayment);
+var counter = 0;
+//Dodanie nowego użytkownika
+function addNewUser() {
+    var userName = document.querySelector(".input-box").value;
+    if (userName == "") {
+        alert("Write something!");
+    }
+    else {
+        counter += 1;
+        var user = new User(userName, counter);
+        usersList.push(user);
+        AddUserToHtml(user);
+        AddToSelect(user);
+        document.querySelector(".input-box").value = "";
+    }
+    console.log(usersList);
+}
+//Dodanie nowej płatnośći
+function addNewPayment() {
+    document.querySelector(".popup").style.display = "block";
+    if (usersList.length > 0) {
+        document.querySelector(".accept-button").style.display = "block";
+    }
+}
+//Dodanie użytkownika do HTML
+function AddUserToHtml(user) {
+    var userDiv = document.querySelector('.users');
+    var newDiv = document.createElement('div');
+    newDiv.className = "user";
+    userDiv.appendChild(newDiv);
+    var newPName = document.createElement('p');
+    newPName.textContent = user.getNickName();
+    newPName.className = "part";
+    newDiv.appendChild(newPName);
+    var newPBalance = document.createElement('p');
+    newPBalance.textContent = user.getBalance() + " zł";
+    newPBalance.className = "part";
+    newPBalance.id = user.getId().toString();
+    newDiv.appendChild(newPBalance);
+}
+//Dodanie użytkownika do listy wyboru potwierdzenia płatnośći
+function AddToSelect(user) {
+    var select = document.querySelector(".popup-select-box");
+    var option = document.createElement('option');
+    option.value = "" + user.getNickName();
+    option.textContent = "" + user.getNickName();
+    select.appendChild(option);
+}
+function CalculatePayment() {
+    var whoPaid = WhoPayed();
+    var filtredListPay = usersList.filter(function (user) { return user.getNickName() == whoPaid; });
+    var filtredListNoPay = usersList.filter(function (user) { return user.getNickName() != whoPaid; });
+    var value = +document.querySelector('.popup-input-box').value;
+    var valueToAdd = value / filtredListNoPay.length;
+    filtredListNoPay.forEach(function (user) { return user.setBalance(-valueToAdd); });
+    filtredListPay.forEach(function (user) { return user.setBalance(value); });
+    usersList.forEach(function (user) { return DataUpdate(user); });
+}
+function WhoPayed() {
+    var selected = document.querySelector(".popup-select-box");
+    return selected.value;
+}
+function DataUpdate(user) {
+    var userBalance = document.getElementById("" + user.getId());
+    userBalance.textContent = user.getBalance() + " zł";
+}
---
+++ script.ts
@@ -0,0 +1,122 @@
+class User {
+    private userId: number;
+    private nickName: string;
+    private balance: number;
+
+    constructor(nickName:string , counter:number){
+        this.nickName = nickName;
+        this.userId = counter;
+        this.balance = 0;
+    }
+    public getNickName():string{
+        return this.nickName;
+    }
+    public getBalance():string{
+        return this.balance.toString();
+    }
+    public getId():number{
+        return this.userId;
+    }
+    public setBalance(change:number)
+    {
+        this.balance += change;
+    }
+}
+
+const usersList: Array <User> = [];  
+
+const addUser = (<HTMLInputElement>document.querySelector(".add-user-button"));
+
+const addPayment = (<HTMLInputElement>document.querySelector(".add-payment-button"));
+
+const confirmPayment = (<HTMLInputElement>document.querySelector('.accept-button'));
+
+(<HTMLInputElement>document.querySelector('.close-btn')).addEventListener('click', ()=>{
+    (<HTMLInputElement>document.querySelector(".popup")).style.display = "none";
+    (<HTMLInputElement>document.querySelector(".popup-input-box")).value = "";
+})
+
+addUser.addEventListener('click',addNewUser);
+
+addPayment.addEventListener('click',addNewPayment);
+
+confirmPayment.addEventListener('click',CalculatePayment);
+
+
+let counter: number = 0;
+
+//Dodanie nowego użytkownika
+function addNewUser(){
+    const userName:string = (<HTMLInputElement>document.querySelector(".input-box")).value;
+    if(userName == ""){
+        alert("Write something!")
+    }
+    else{
+        counter += 1;
+        const user:User = new User(userName, counter);
+        usersList.push(user);
+        AddUserToHtml(user);
+        AddToSelect(user);
+        (<HTMLInputElement>document.querySelector(".input-box")).value = "";
+    }
+    console.log(usersList);
+}
+
+//Dodanie nowej płatnośći
+function addNewPayment()
+{
+    (<HTMLInputElement>document.querySelector(".popup")).style.display = "block";
+    if(usersList.length>0){
+        (<HTMLInputElement>document.querySelector(".accept-button")).style.display = "block";
+    }
+}
+
+//Dodanie użytkownika do HTML
+function AddUserToHtml(user:User)
+{   
+    const userDiv = (<HTMLInputElement>document.querySelector('.users'));
+    const newDiv = (<HTMLInputElement>document.createElement('div'));
+    newDiv.className = "user";
+    userDiv.appendChild(newDiv);
+    const newPName = document.createElement('p')
+    newPName.textContent = user.getNickName();
+    newPName.className = "part";
+    newDiv.appendChild(newPName);
+    const newPBalance = document.createElement('p')
+    newPBalance.textContent = user.getBalance() + " zł";
+    newPBalance.className = "part";
+    newPBalance.id = user.getId().toString();
+    newDiv.appendChild(newPBalance);
+}
+
+//Dodanie użytkownika do listy wyboru potwierdzenia płatnośći
+function AddToSelect(user:User){
+    const select = (<HTMLInputElement>document.querySelector(".popup-select-box"));
+    let option = document.createElement('option');
+    option.value = `${user.getNickName()}`;
+    option.textContent = `${user.getNickName()}`;
+    select.appendChild(option);
+}
+
+function CalculatePayment(){
+    const whoPaid:string = WhoPayed();
+    const filtredListPay:Array <User> = usersList.filter(user => user.getNickName() == whoPaid);
+    const filtredListNoPay:Array<User> = usersList.filter(user => user.getNickName() != whoPaid)
+    const value:number = +(<HTMLInputElement>document.querySelector('.popup-input-box')).value;
+    const valueToAdd = value/filtredListNoPay.length;
+    filtredListNoPay.forEach(user => user.setBalance(-valueToAdd))
+    filtredListPay.forEach(user => user.setBalance(value));
+    usersList.forEach(user => DataUpdate(user));
+
+}
+
+function WhoPayed():string{
+    const selected = (<HTMLInputElement>document.querySelector(".popup-select-box"));
+    return selected.value;
+}
+
+function DataUpdate(user:User)
+{
+    const userBalance = (<HTMLInputElement>document.getElementById(`${user.getId()}`));
+    userBalance.textContent = user.getBalance() + " zł";
+}
